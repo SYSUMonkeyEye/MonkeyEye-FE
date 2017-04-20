92dd383741b5df4b90327bd8dc7b22091436d24f
陈平永 <734162700@qq.com>
2017-04-20 16:29:56 +0800

    实现登录部分页面

--- src/components/auth/Signin.vue
+++ src/components/auth/Signin.vue
@@ -3,6 +3,19 @@ div#signin
   md-toolbar
     div.md-toolbar-container
       h2.md-title 账户登录
+  form#form(novalidate, @submit.stop.prevent='submit')
+    md-input-container
+      md-icon phone
+      label 手机号
+      md-input(type='text' required)
+      span.md-error Validation message
+    md-input-container
+      md-icon vpn_key
+      label 输入密码
+      md-input(type='password' required)
+      span.md-error Validation message
+    md-button.md-raised.md-primary#login 登录
+
 </template>
 
 <script>
@@ -16,4 +29,11 @@ export default {
   .md-toolbar .md-toolbar-container .md-title
     flex: 1
     text-align: center
+  #form
+    width: 80%
+    margin: 0 auto
+    #login
+      display: block
+      width: 80%
+      margin: 0 auto
 </style>
