53ecf15f007691ac5df096112e236a9f218aaec7
hongchh <hongchh_sysu@qq.com>
2017-04-27 09:33:00 +0800

    完成电影评价界面

--- src/components/comment/MovieComment.vue
+++ src/components/comment/MovieComment.vue
@@ -2,18 +2,72 @@
 div#movie-comment
   md-toolbar
     div.md-toolbar-container
-      h2.md-title 评价界面
+      md-button.md-icon-button
+        md-icon arrow_left
+      h2.md-title 速度与激情8
+      md-button.md-icon-button(@click.native="submit")
+        md-icon check
+  div.star-rating
+    div.rating
+      span 我的评分：
+      span {{ rating }}
+    div.stars
+      img(src="../../assets/images/star-on.png", v-for="i in rating", :id="i", @click="setRating($event)")
+      img(src="../../assets/images/star-off.png", v-for="i in (5 - rating)", :id="i + rating", @click="setRating($event)")
+  textarea.content(v-model="content", placeholder="分享你的观看心得吧~")
 </template>
 
 <script>
 export default {
-  name: 'movie-comment'
+  name: 'movie-comment',
+  data () {
+    return {
+      rating: 1,
+      content: ''
+    }
+  },
+  methods: {
+    setRating (event) {
+      this.rating = Number.parseInt(event.target.id)
+    },
+    submit () {
+      console.log(this.rating, this.content)
+    }
+  }
 }
 </script>
 
 <style lang="sass">
 #movie-comment
-  .md-toolbar .md-toolbar-container .md-title
-    flex: 1
-    text-align: center
+  position: relative
+  height: 100%
+  background: #eee
+  .md-toolbar
+    height: .64rem
+    .md-toolbar-container .md-title
+      flex: 1
+      text-align: center
+  .star-rating
+    background: white
+    margin-top: .1rem
+    padding: .15rem
+    .rating span
+      font-size: .24rem
+      &:last-child
+        color: #f44336
+    .stars
+      display: flex
+      justify-content: space-around
+      margin-top: .1rem
+      img
+        width: .4rem
+        height: .4rem
+  .content
+    width: 100%
+    height: 3rem
+    margin-top: .1rem
+    outline: none
+    padding: .1rem
+    font-size: .16rem
+    resize: none
 </style>
