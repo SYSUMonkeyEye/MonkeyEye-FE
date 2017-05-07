3df777dec4bfde7d7bd7a8ef6e7f889a1552312f
骆铭涛 <513301360@qq.com>
2017-05-07 11:13:45 +0800

    加入日期工具函数

---
+++ src/components/order/SelectSeat.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="sass">
+</style>
---
+++ src/utils/DateUtils.js
@@ -0,0 +1,72 @@
+/**
+ * @param {Date} date - 日期
+ * @returns {Date} 当天零点
+ */
+function formatDate (date) {
+  return new Date(date.getFullYear() + '-' +
+    (date.getMonth() + 1) + '-' + date.getDate())
+}
+
+/**
+ * 计算两天相差天数
+ * @param {Date} dateOne
+ * @param {Date} dateTwo
+ * @returns {Number}
+ */
+function daysBetween (dateOne, dateTwo) {
+  let _dateOne = formatDate(dateOne)
+  let _dateTwo = formatDate(dateTwo)
+
+  let diff = _dateTwo.getTime() - _dateOne.getTime()
+  let days = parseInt(diff / (1000 * 60 * 60 * 24), 10)
+
+  return days
+}
+
+/**
+ * @param {number} offset - 偏离天数
+ * @returns {string} 与当前日期偏离offset的日期
+ */
+function getDate (offset = 0) {
+  let now = new Date()
+  now.setDate(now.getDate() + offset)
+
+  let m = now.getMonth() + 1
+  let d = now.getDate()
+
+  return m + '月' + d + '日'
+}
+
+/**
+ * 为小于10的数字前面添加0
+ * @param n
+ * @returns {string}
+ */
+function addZero (n) {
+  if (n < 10) {
+    return '0' + n
+  }
+  return '' + n
+}
+
+/**
+ * @param {number} time - 时间戳
+ * @param {number} offset - 偏离分钟
+ * @returns {string} 格式为HH:mm的时间
+ */
+function getTime (time, offset = 0) {
+  let t = new Date(time)
+  t.setMinutes(t.getMinutes() + offset)
+
+  let h = addZero(t.getHours())
+  let m = addZero(t.getMinutes())
+
+  return h + ':' + m
+}
+
+export {
+  formatDate,
+  daysBetween,
+  getDate,
+  getTime
+}
