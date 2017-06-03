c6ff05378db6a5f1948c98fc1eb9dc02855b0e78
hongchh <hongchh_sysu@qq.com>
2017-06-03 21:03:35 +0800

    增加代理服务器地址配置

--- app-server.js
+++ app-server.js
@@ -2,13 +2,14 @@ const port = 8080
 const express = require('express')
 const path = require('path')
 const proxyMiddleware = require('http-proxy-middleware')
+const config = require('./config')
 
 let app = express()
 
 app.use('/static/js', express.static(path.join(__dirname, './dist/static/js')))
 app.use('/static/css', express.static(path.join(__dirname, './dist/static/css')))
-app.use(proxyMiddleware('/api', { target: 'http://123.207.233.226:1234' }))
-app.use(proxyMiddleware('/static', { target: 'http://123.207.233.226:1234' }))
+app.use(proxyMiddleware('/api', { target: config.apiServer }))
+app.use(proxyMiddleware('/static', { target: config.assetsServer }))
 app.use('/dist/index.html', express.static(path.join(__dirname, './dist/index.html')))
 app.use('*', (req, res) => {
   res.redirect('/dist/index.html')
