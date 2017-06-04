b6e8d629959a08286d877cd2881698bf49083802
骆铭涛 <513301360@qq.com>
2017-06-05 01:05:38 +0800

    重新开启API代理

--- app-server.js
+++ app-server.js
@@ -1,6 +1,8 @@
 const port = 8080
 const express = require('express')
 const path = require('path')
+const proxyMiddleware = require('http-proxy-middleware')
+const config = require('./config')
 const fs = require('fs')
 const compression = require('compression')
 
@@ -11,6 +13,8 @@ let app = express()
 app.use(compression())
 app.use('/static/js', express.static(path.join(__dirname, './dist/static/js')))
 app.use('/static/css', express.static(path.join(__dirname, './dist/static/css')))
+app.use('/api', proxyMiddleware({target: config.apiServer, changeOrigin: true}))
+app.use('/static', proxyMiddleware({target: config.assetsServer, changeOrigin: true}))
 
 app.use('*', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' })
--- config/index.js
+++ config/index.js
@@ -39,5 +39,7 @@ module.exports = {
     // In our experience, they generally work as expected,
     // just be aware of this issue when enabling this option.
     cssSourceMap: false
-  }
+  },
+  apiServer: apiServer,
+  assetsServer: assetsServer
 }
