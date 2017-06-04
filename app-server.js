a6f462af1de5e58f81ad5714fab06abf3df22a69
hongchh <hongchh_sysu@qq.com>
2017-06-04 10:49:40 +0800

    完善应用服务器代码

--- app-server.js
+++ app-server.js
@@ -3,6 +3,9 @@ const express = require('express')
 const path = require('path')
 const proxyMiddleware = require('http-proxy-middleware')
 const config = require('./config')
+const fs = require('fs')
+
+const index = fs.readFileSync(path.join(__dirname, './dist/index.html'))
 
 let app = express()
 
@@ -10,9 +13,9 @@ app.use('/static/js', express.static(path.join(__dirname, './dist/static/js')))
 app.use('/static/css', express.static(path.join(__dirname, './dist/static/css')))
 app.use(proxyMiddleware('/api', { target: config.apiServer }))
 app.use(proxyMiddleware('/static', { target: config.assetsServer }))
-app.use('/dist/index.html', express.static(path.join(__dirname, './dist/index.html')))
 app.use('*', (req, res) => {
-  res.redirect('/dist/index.html')
+  res.writeHead(200, {"Content-Type":"text/html"})
+  res.end(index)
 })
 
 app.listen(port)
