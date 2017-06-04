a883033bb60a145c17274e0ca9085f9ae9d68091
骆铭涛 <513301360@qq.com>
2017-06-05 00:53:37 +0800

    关闭api代理；开启压缩

--- app-server.js
+++ app-server.js
@@ -1,23 +1,22 @@
 const port = 8080
 const express = require('express')
 const path = require('path')
-const proxyMiddleware = require('http-proxy-middleware')
-const config = require('./config')
 const fs = require('fs')
+const compression = require('compression')
 
 const index = fs.readFileSync(path.join(__dirname, './dist/index.html'))
 
 let app = express()
 
+app.use(compression())
 app.use('/static/js', express.static(path.join(__dirname, './dist/static/js')))
 app.use('/static/css', express.static(path.join(__dirname, './dist/static/css')))
-app.use(proxyMiddleware('/api', { target: config.apiServer }))
-app.use(proxyMiddleware('/static', { target: config.assetsServer }))
+
 app.use('*', (req, res) => {
-  res.writeHead(200, {"Content-Type":"text/html"})
+  res.writeHead(200, { 'Content-Type': 'text/html' })
   res.end(index)
 })
 
 app.listen(port)
 
-console.log("http://localhost:" + port)
+console.log('http://localhost:' + port)
